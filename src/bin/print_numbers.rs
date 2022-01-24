ea1a55a0ae13e854990e5fbf4b720de6a3fd8ad2
TheAwiteb <awiteb@hotmail.com>
2022-01-24 18:48:09 +0300

    update to 1.58.0 🔥

--- src/bin/print_numbers.rs
+++ src/bin/print_numbers.rs
@@ -3,7 +3,7 @@
 /// Returns the max length of numbers in the given array
 /// between decimal, hexadecimal and octal
 fn get_max_len(arr: &Vec<u64>) -> usize {
-    let max_dec = arr.iter().max().expect("The array  is empty!");
+    let max_dec = arr.iter().max().expect("The array is empty!");
     *[
         max_dec.to_string().len(),      // len of decimal
         format!("{:x}", max_dec).len(), // len of hexadecimal
@@ -26,8 +26,6 @@ fn main() {
             \rNumber in hexadecimal {num:0>max_digit$x}
             \rNumber in octal       {num:0>max_digit$o}
             ",
-            num = num,
-            max_digit = max_digit
         );
     }
 }
